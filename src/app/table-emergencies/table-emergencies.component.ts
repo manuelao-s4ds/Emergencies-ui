1d37fc9b979a2ce0becbefb32988510a94be5026
leo <leonardo.vargas@ottohgealthcare.com>
2017-07-12 09:48:22 -0500

    the emergency table component is created

---
+++ src/app/table-emergencies/table-emergencies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-emergencies',
+  templateUrl: './table-emergencies.component.html',
+  styleUrls: ['./table-emergencies.component.css']
+})
+export class TableEmergenciesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
