4c0dced8d83ccd9230caeaa68f80be88654c2fd8
leo <leonardo.vargas@ottohgealthcare.com>
2017-07-12 21:37:24 -0500

    the modal-edit-paramedics component is created

---
+++ src/app/modals/modal-edit-paramedics/modal-edit-paramedics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-edit-paramedics',
+  templateUrl: './modal-edit-paramedics.component.html',
+  styleUrls: ['./modal-edit-paramedics.component.css']
+})
+export class ModalEditParamedicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
