241a4b8b85f8bd81e8029c1b78dc4b37bd47b827
leo <leonardo.vargas@ottohgealthcare.com>
2017-07-12 23:53:14 -0500

    <table-paramedics.component.ts>
    the functionality from component is created

--- src/app/paramedics/table-paramedics/table-paramedics.component.ts
+++ src/app/paramedics/table-paramedics/table-paramedics.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import {ToastsManager} from 'ng2-toastr';
+import { ParamedicsService } from '../paramedics/paramedics.services';
+import { ModalEditParamedicsService } from '../../modals/modal-edit-paramedics/modal-edit-paramedics.service';
 
 @Component({
   selector: 'app-table-paramedics',
   templateUrl: './table-paramedics.component.html',
-  styleUrls: ['./table-paramedics.component.css']
+  styleUrls: ['./table-paramedics.component.css'],
+  providers: [ModalEditParamedicsService, ParamedicsService]
 })
 export class TableParamedicsComponent implements OnInit {
 
-  constructor() { }
+  public paramedics: any [];
+  public titleModal: string;
+
+  constructor(
+    private _paramedicsService: ParamedicsService,
+    private _container: ViewContainerRef,
+    private _toast: ToastsManager,
+    private _modalEditParamedicsService: ModalEditParamedicsService
+  ) {
+    this.paramedics = [];
+    this.titleModal = '';
+    this._toast.setRootViewContainerRef(_container);
+  }
 
   ngOnInit() {
+    this.titleModal = 'Modificar Paramedico';
+    this.getParamedics();
+  }
+  getParamedics() {
+    this._paramedicsService.list().subscribe(
+      (res) => {
+        this.paramedics = res.json();
+      },
+      (err) => {
+        this.paramedics = [];
+        this._toast.error(`Ocurrio un error al obtener los paramedicos`, 'Paramedicos!');
+        console.log(err.json());
+      }
+    );
+  }
+
+  openModal(data) {
+    this._modalEditParamedicsService.openModal(this.titleModal, data)
+      .subscribe(
+        (res: any) => {
+          if (res.valid) {
+            this._paramedicsService.update(res.paramedic).subscribe(
+              (response) => {
+                this.getParamedics();
+                this._toast.success(`Se actualizó el paramedico`, 'Paramedicos!');
+              },
+              (error) => {
+                this._toast.error(`Ocurrio un error al actualuzar el paramedico`, 'Paramedicos!');
+                console.log(error.json());
+              }
+            );
+          }
+        }
+      );
+  }
+  delete(paramedic) {
+    this._paramedicsService.delete(paramedic._id).subscribe(
+      (response) => {
+        this.getParamedics();
+        this._toast.success(`Se elimino el paramedico`, 'Paramedicos!');
+      },
+      (error) => {
+        this._toast.error(`Ocurrio un error al eliminar el paramedico`, 'Paramedicos!');
+        console.log(error.json());
+      }
+    );
   }
 
 }
