9c3fba20a9e7ce060a32da29ed8ae87fc5a7410c
leo <leonardo.vargas@ottohgealthcare.com>
2017-07-12 21:36:06 -0500

    the table-paramedicts component is created

---
+++ src/app/paramedics/table-paramedics/table-paramedics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-paramedics',
+  templateUrl: './table-paramedics.component.html',
+  styleUrls: ['./table-paramedics.component.css']
+})
+export class TableParamedicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
