727c16bab057d1e77e8aa177aa2a5b48dc88ec87
leo <leonardo.vargas@ottohgealthcare.com>
2017-07-12 23:40:56 -0500

    <routes.ts>
    add new routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth-guard/auth-guard.guard';
 import { MainWindowComponent } from './main-window/main-window.component';
+import { ParamedicsComponent } from './paramedics/paramedics/paramedics.component';
 import { EmergencyComponent } from './emergencies/emergency/emergency.component';
+import { TableParamedicsComponent } from './paramedics/table-paramedics/table-paramedics.component';
 import { TableEmergenciesComponent } from './emergencies/table-emergencies/table-emergencies.component';
 
 
@@ -21,6 +23,14 @@ export const routeConfig: Routes = [
             {
               path: 'listado-emergencias',
               component: TableEmergenciesComponent
+            },
+            {
+              path: 'paramedicos',
+              component: ParamedicsComponent
+            },
+            {
+              path: 'listado-paramedicos',
+              component: TableParamedicsComponent
             }
           ]
         }
