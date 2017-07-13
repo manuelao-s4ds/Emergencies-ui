7fef8f976541d30e6b0b0bf3d3d00aa6b6c4192d
leo <leonardo.vargas@ottohgealthcare.com>
2017-07-12 23:48:04 -0500

    <paramedics.comp[onent.ts>
    the functionality from component is created

--- src/app/paramedics/paramedics/paramedics.component.ts
+++ src/app/paramedics/paramedics/paramedics.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import {ToastsManager} from 'ng2-toastr';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { ParamedicsService } from './paramedics.services';
 
 @Component({
-  selector: 'app-paramedics',
+  selector: 'app-emergency',
   templateUrl: './paramedics.component.html',
-  styleUrls: ['./paramedics.component.css']
+  styleUrls: ['./paramedics.component.css'],
+  providers: [ParamedicsService]
 })
 export class ParamedicsComponent implements OnInit {
 
-  constructor() { }
+  formParamedics: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private _paramedicsService: ParamedicsService,
+    private _container: ViewContainerRef,
+    private _toast: ToastsManager
+  ) {
+    this._toast.setRootViewContainerRef(_container);
+  }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.formParamedics = this.fb.group({ // <-- the parent FormGroup
+      name: ['', Validators.required ],
+      lastname: ['', Validators.required],
+      phone: '',
+      cellPhone: ['', Validators.required ],
+      gender: ['', Validators.required ],
+      birthdate: '',
+      specialization: ['', Validators.required ]
+    });
+  }
+  save() {
+    if (this.formParamedics.valid) {
+      const paramedic = {
+        name: this.formParamedics.get('name').value,
+        lastname: this.formParamedics.get('lastname').value,
+        phone: this.formParamedics.get('phone').value,
+        cellPhone: this.formParamedics.get('cellPhone').value,
+        gender: this.formParamedics.get('gender').value,
+        birthdate: this.formParamedics.get('birthdate').value,
+        specialization: this.formParamedics.get('specialization').value
+      };
+      this._paramedicsService.save(paramedic).subscribe(
+        (res) => {
+          this._toast.success(`Se registro el paramedico ${res.json().name}`, 'Paramedicos!');
+          this.formParamedics.reset({
+            gender: ''
+          })
+        },
+        (error) => {
+          console.log(error.json());
+          this._toast.error(`Ocurrio un error al registrar al paramedico`, 'Paramedicos!');
+        }
+      );
+    }else {
+      this._toast.info(`Todos los campos marcados con * son obligatorios`, 'Paramedicos!');
+
+    }
   }
 
 }
