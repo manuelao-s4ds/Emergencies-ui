21132e1706ea3354f0d658a546eb01a517bbb4d3
leo <leonardo.vargas@ottohgealthcare.com>
2017-07-12 21:36:46 -0500

    the paramedicts component is created

---
+++ src/app/paramedics/paramedics/paramedics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paramedics',
+  templateUrl: './paramedics.component.html',
+  styleUrls: ['./paramedics.component.css']
+})
+export class ParamedicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
